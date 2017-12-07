aa6ad450357c3122ccbd06716b948da50d2d8d2e
DorisQian <qianyh0806@163.com>
2017-12-07 23:48:12 +0800

    update MinutesToHours.py

--- MinutesToHours.py
+++ MinutesToHours.py
@@ -3,19 +3,25 @@
 import sys
 
 def Hours():
-	minute = int(input())
+	minute = int(sys.argv[1])
 	try:
-		hour = minute // 60
-		minute2 = minute % 60
+		if minute > 0:
+			hour = minute // 60
+			minute2 = minute % 60
+			print("{} H, {} M".format(hour,minute2))
+		else:
+			raise ValueError
 	except ValueError:
 		print("ValueError：Input number cannot be negative")
-	return hour, minute2
-	print(" {} H. {} M".formate(hour, minute2)
 
 if __name__ == '__main__':
-	if len(sys.argv) > 2:
-		Hours()
-	else:
+	try:
+		if len(sys.argv) > 1:
+			Hours()
+		else:
+			raise ValueError
+	except ValueError:
+		print("缺少参数")		
 		sys.exit(-1)
 	sys.exit(0)
 		

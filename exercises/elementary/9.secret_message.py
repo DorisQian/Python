13e56fb7a38da816680468b32dac6d703c5331b7
Doris <qianyh0806@163.com>
2017-12-17 21:21:28 +0800

    get the upper words

--- exercises/elementary/9.secret_message.py
+++ exercises/elementary/9.secret_message.py
@@ -11,7 +11,7 @@ Output: The secret message as a string or an empty string.
 
 给你一个文本块。按照出现在文本中的顺序将所有大写字母收集在一个单词中。
 
-例如：文本=“ ^ h流量是你吗？ê小时，OK。大号流或大号奥尔？ Ø HHH”，如果我们收集所有的大写字母，我们得到的消息“HELLO”。
+例如：文本=“How are you? Eh, ok. Low or Lower? Ohhh.”，如果我们收集所有的大写字母，我们得到的消息“HELLO”。
 
 输入：文本为字符串（unicode）。
 
@@ -34,7 +34,12 @@ all(ch in string.printable for ch in text)
 
 def find_message(text):
     """Find a secret message"""
-    return ""
+    final = ""
+    for up in text:
+    	if up.isupper():
+    		final += up
+    return final
+
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing

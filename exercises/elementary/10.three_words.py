43454dcd7e898a62c7c96bc7753a9a1512e83409
Doris <qianyh0806@163.com>
2017-12-17 21:23:17 +0800

    judge if there have a number in a string

--- exercises/elementary/10.three_words.py
+++ exercises/elementary/10.three_words.py
@@ -28,13 +28,19 @@ checkio("Hi") == False
 """
 
 def checkio(words):
-    return True or False
-
+    word = words.split()
+    if len(word) < 3:
+    	return False
+    else:
+    	for wo in word:
+    		if wo.isalpha() is False:
+    			return False
+    	return True
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
-    assert checkio("Hello World hello") == True, "Hello"
-    assert checkio("He is 123 man") == False, "123 man"
-    assert checkio("1 2 3 4") == False, "Digits"
-    assert checkio("bla bla bla bla") == True, "Bla Bla"
-    assert checkio("Hi") == False, "Hi"
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+	assert checkio("Hello World hello") == True, "Hello"
+	assert checkio("He is 123 man") == False, "123 man"
+	assert checkio("1 2 3 4") == False, "Digits"
+	assert checkio("bla bla bla bla") == True, "Bla Bla"
+	assert checkio("Hi") == False, "Hi"
+    

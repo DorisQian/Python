60eed6e028d405fce2442f50ca7bc312ecf7a427
Doris <qianyh0806@163.com>
2017-12-19 00:19:25 +0800

    get a new tuple

--- exercises/elementary/17.easy_unpack.py
+++ exercises/elementary/17.easy_unpack.py
@@ -11,7 +11,7 @@ easy_unpack((1, 2, 3, 4, 5, 6, 7, 9)) == (1, 3, 7)
 easy_unpack((1, 1, 1, 1)) == (1, 1, 1)
 easy_unpack((6, 3, 7)) == (6, 7, 3)
 
-你的任务是创建一个函数，获取一个元组并返回一个包含3个元素的元组 - 第一个，第三个和第二个到给定元组的最后一个
+你的任务是创建一个函数，获取一个元组并返回一个包含3个元素的元组 - 第一个，第三个和倒数第二个
 
 输入：一个元组，至少3个元素长。
 
@@ -24,7 +24,14 @@ def easy_unpack(elements):
         returns a tuple with 3 elements - first, third and second to the last
     """
     # your code here
-    return ()
+ #   new_list = ''.join(str(elements[:3:2]))
+ #   ele = new_list + (str(elements[-2],))
+ #   print(ele)
+ #   print(new_list)
+ #   return ele
+
+    return (elements[0],) + (elements[2],) + (elements[-2],)
+    
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing

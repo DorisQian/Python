fa1215880a064a733966efe9871099baeb77fc5f
Doris <qianyh0806@163.com>
2017-12-18 21:53:17 +0800

    convert number into decimal with base

--- exercises/elementary/14.number_base.py
+++ exercises/elementary/14.number_base.py
@@ -34,21 +34,25 @@ re.match("\A[A-Z0-9]\Z", str_number)
 
 """
 
-def checkio(number):
+def checkio(str_number, radix):
     #Your code here
     #It's main function. Don't remove this function
     #It's using for auto-testing and must return a result for check.
 
     #replace this for solution
-    return str(number)
+    try:
+        return(int(str_number, radix))
+    except ValueError:
+        return -1
 
 #Some hints:
 #Convert a number in the string with str(n)
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
-    assert checkio(15) == "Fizz Buzz", "15 is divisible by 3 and 5"
-    assert checkio(6) == "Fizz", "6 is divisible by 3"
-    assert checkio(5) == "Buzz", "5 is divisible by 5"
-    assert checkio(7) == "7", "7 is not divisible by 3 or 5"
+    assert checkio("AF", 16) == 175,'AF'
+    assert checkio("101", 2) == 5,'1012'
+    assert checkio("101", 5) == 26,'1015'
+    assert checkio("Z", 36) == 35,'Z'
+    assert checkio("AB", 10) == -1,'AB'
     print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")

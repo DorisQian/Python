b94b117affe8849945310d2104fb31fcb46b6e3a
DorisQian <qianyh@163.com>
2018-02-09 17:45:08 +0800

    solve o'reilly 6,8,9

--- checkio/o'reilly/6.ghosts_age.py
+++ checkio/o'reilly/6.ghosts_age.py
@@ -1,7 +1,13 @@
 """
-Nicola takes a moment to study the ghosts. He is trying to think up a new method to determine just how old these ghosts are. He knows that their age is somehow related to their opacity. To measure their opacity Nikola uses a scale of 10000 units to 0 units, where 10000 is a completely opaque newborn ghost (0 years old) and 0 is completely transparent ghost (of an unknown age).
+Nicola takes a moment to study the ghosts. He is trying to think up a new method to 
+determine just how old these ghosts are. He knows that their age is somehow related 
+to their opacity. To measure their opacity Nikola uses a scale of 10000 units to 0 
+units, where 10000 is a completely opaque newborn ghost (0 years old) and 0 is completely 
+transparent ghost (of an unknown age).
 
-After some experimenting, Nikola thinks he has discovered the law of ghostly opacity. On every birthday, a ghost's opacity is reduced by a number of units equal to its age when its age is a Fibonacci number (Read about this here) or increased by 1 if it is not.
+After some experimenting, Nikola thinks he has discovered the law of ghostly opacity. 
+On every birthday, a ghost's opacity is reduced by a number of units equal to its age 
+when its age is a Fibonacci number (Read about this here) or increased by 1 if it is not.
 
 For example:
 A newborn ghost -- 10000 units of opacity.
@@ -10,9 +16,12 @@ A newborn ghost -- 10000 units of opacity.
 3 year -- 9997 - 3 = 9994 (3 is a Fibonacci number).
 4 year -- 9994 + 1 = 9995 (4 is not a Fibonacci number).
 5 year -- 9995 - 5 = 9990 (5 is a Fibonacci number).
-Help Nicola write a function which will determine the age of a ghost based on its opacity. You are given opacity measurements as a number ranging from 0 to 10000 inclusively. The ghosts cannot be older than 5000 years as they simply disappear after such a long time (really!).
+Help Nicola write a function which will determine the age of a ghost based on its opacity. 
+You are given opacity measurements as a number ranging from 0 to 10000 inclusively. The 
+ghosts cannot be older than 5000 years as they simply disappear after such a long time (really!).
 
-This is a Halloween task so you should try and write a "magic" or "scary" solution. Please, do not write "regular" solution.
+This is a Halloween task so you should try and write a "magic" or "scary" solution. Please, 
+do not write "regular" solution.
 
 Input: An opacity measurement as an integer.
 
@@ -27,15 +36,20 @@ checkio(9994) == 3
 checkio(9995) == 4
 checkio(9990) == 5
 
-How it is used: This task was made for some spooky fun! We hope to see your funny, tricky and creative solutions!
+How it is used: This task was made for some spooky fun! We hope to see your funny, tricky and 
+creative solutions!
 
 Precondition:
 age < 5000
 0 < opacity ≤ 10000
 
-尼古拉花了一些时间研究这些鬼魂。他试图想出一个新方法来确定这些鬼的年龄。他知道他们的年龄与他们的不透明有关。为了测量它们的不透明度，Nikola使用了10000个单位到0个单位的比例，其中10000是一个完全不透明的新生鬼(0岁)，0是完全透明的鬼(一个未知的年龄)。
-经过一些实验，尼古拉认为他已经发现了幽灵般的不透明的规律。在每个生日的时候，一个幽灵的不透明度会被一些单位所减少，当它的年龄是一个斐波那契数的时候(在这里读到这个)，或者如果不是的话，它会增加1个。
-帮助Nicola编写一个函数，根据它的不透明度确定一个鬼的年龄。你的不透明度测量值从0到10000不等。这些鬼魂不可能超过5000年，因为它们在这么长的时间里消失了(真的!)
+尼古拉花了一些时间研究这些鬼魂。他试图想出一个新方法来确定这些鬼的年龄。他知道他们的年龄与他
+们的不透明有关。为了测量它们的不透明度，Nikola使用了10000个单位到0个单位的比例，其中10000是
+一个完全不透明的新生鬼(0岁)，0是完全透明的鬼(一个未知的年龄)。
+经过一些实验，尼古拉认为他已经发现了幽灵般的不透明的规律。在每个生日的时候，一个幽灵的不透明
+度会被一些单位所减少，当它的年龄是一个斐波那契数的时候(在这里读到这个)，或者如果不是的话，它会增加1个。
+帮助Nicola编写一个函数，根据它的不透明度确定一个鬼的年龄。你的不透明度测量值从0到10000不等。
+这些鬼魂不可能超过5000年，因为它们在这么长的时间里消失了(真的!)
 这是一个万圣节的任务，所以你应该尝试写一个“魔法”或“可怕的”解决方案。请不要写“常规”的解决方案。
 
 输入:作为整数的不透明度度量。
@@ -43,14 +57,71 @@ age < 5000
 
 如何使用:这个任务是为了一些令人毛骨悚然的乐趣!我们希望看到你的有趣，狡猾和创造性的解决方案!
 """
+
+def fibonacci(n):
+    if n == 0:
+    	return 0
+    elif n == 1:
+    	return 1
+    else:
+    	return fibonacci(n - 1) + fibonacci(n - 2)
+
+fibonacci_seq = []
+for n in range(20):
+	fibonacci_seq.append(fibonacci(n))
+print(fibonacci_seq)
+
+def get_opacity(age):
+	if age == 0:
+		return 10000
+	elif age == 990:
+		return 8393
+	elif age == 1890:
+		return 7695
+	elif age == 2790:
+		return 6010
+	elif age == 3690:
+		return 6910
+	elif age == 4590:
+		return 3628
+	elif age in fibonacci_seq:
+		return get_opacity(age - 1) - age
+	else:
+		return get_opacity(age - 1) + 1
+
 def checkio(opacity):
-    return 0
+	opacity_dic = {}
+	for age in range(990):
+		opacity_dic[get_opacity(age)] = age
+		
+	for age in range(990, 1890):
+		opacity_dic[get_opacity(age)] = age
+
+	for age in range(1890, 2790):
+		opacity_dic[get_opacity(age)] = age
+
+	for age in range(2790, 3690): 
+		opacity_dic[get_opacity(age)] = age
+
+	for age in range(3690, 4590):
+		opacity_dic[get_opacity(age)] = age
+
+	for age in range(4590, 5001):
+		opacity_dic[get_opacity(age)] = age
+
+	print(opacity_dic[opacity])
+	return opacity_dic[opacity]
+
+
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
+	
     assert checkio(10000) == 0, "Newborn"
     assert checkio(9999) == 1, "1 year"
     assert checkio(9997) == 2, "2 years"
     assert checkio(9994) == 3, "3 years"
     assert checkio(9995) == 4, "4 years"
     assert checkio(9990) == 5, "5 years"
+    assert checkio(3703) == 4665
+    
--- checkio/o'reilly/9.create_intervals.py
+++ checkio/o'reilly/9.create_intervals.py
@@ -1,14 +1,20 @@
 """
-From a set of ints you have to create a list of closed intervals as tuples, so the intervals are covering all the values found in the set.
+From a set of ints you have to create a list of closed intervals as tuples, so 
+the intervals are covering all the values found in the set.
 
-A closed interval includes its endpoints! The interval 1..5, for example, includes each value x that satifies the condition 1 <= x <= 5.
+A closed interval includes its endpoints! The interval 1..5, for example, includes 
+each value x that satifies the condition 1 <= x <= 5.
 
-Values can only be in the same interval if the difference between a value and the next smaller value in the set equals one, otherwise a new interval begins. Of course, the start value of an interval is excluded from this rule.
-A single value, that does not fit into an existing interval becomes the start- and endpoint of a new interval.
+Values can only be in the same interval if the difference between a value and the 
+next smaller value in the set equals one, otherwise a new interval begins. Of course, 
+the start value of an interval is excluded from this rule.
+A single value, that does not fit into an existing interval becomes the start- and 
+endpoint of a new interval.
 
 Input: A set of ints.
 
-Output: A list of tuples of two ints, indicating the endpoints of the interval. The Array should be sorted by start point of each interval
+Output: A list of tuples of two ints, indicating the endpoints of the interval. The 
+Array should be sorted by start point of each interval
 
 Examples:
 
@@ -17,21 +23,46 @@ create_intervals({1, 2, 3, 6, 7, 8, 4, 5}) == [(1, 8)]
 
 从一组ints中，您必须创建一个作为元组的闭区间列表，因此间隔将覆盖集合中发现的所有值。
 闭区间包括它的端点!间隔1 . .例如，包含满足条件1 <= x <= 5的每个值x。
-如果值与集合中的下一个较小值之间的差值为1，则值只能在相同的间隔内，否则将开始新的间隔。当然，间隔的开始值被排除在这个规则之外。
+如果值与集合中的下一个较小值之间的差值为1，则值只能在相同的间隔内，否则将开始新的间隔。
+当然，间隔的开始值被排除在这个规则之外。
 一个单独的值，它不适合于一个现有的间隔，成为一个新的间隔的开始和终点。
 
 输入:一组输入。
 输出:两个ints的元组列表，表示间隔的端点。每个间隔的起始点应该对数组进行排序
 """
+
+
 def create_intervals(data):
-    """
-        Create a list of intervals out of set of ints.
-    """
-    # your code here
-    return None
+	sort_list = sorted(data)
+	if len(sort_list) == 0:
+		return []
+	tu = ()
+	result = []
+	beg = sort_list[0]
+	end = 0
+	for i in range(len(sort_list) - 1):
+		if sort_list[i + 1] - sort_list[i] != 1:
+			end  = sort_list[i]
+			tu = (beg, end)
+			beg = sort_list[i + 1]
+			result.append(tu)
+		
+	if end == 0:
+		end = sort_list[len(sort_list) - 1]	
+	elif sort_list.index(end) < sort_list.index(beg) :
+		end = sort_list[len(sort_list) - 1]
+
+	tu = (beg, end)
+	result.append(tu)
+
+	print(result)
+	return result
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     assert create_intervals({1, 2, 3, 4, 5, 7, 8, 12}) == [(1, 5), (7, 8), (12, 12)], "First"
     assert create_intervals({1, 2, 3, 6, 7, 8, 4, 5}) == [(1, 8)], "Second"
+    assert create_intervals([1,3,7]) == [(1, 1), (3, 3), (7, 7)]
+    assert create_intervals([7,9,10,11,12]) == [(7,7),(9,12)]
+    assert create_intervals([]) == []
     print('Almost done! The only thing left to do is to Check it!')

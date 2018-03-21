62046ee9d7ea1dcac5ec77d1cc6e209238c8add7
DorisQian <qianyh@163.com>
2018-03-21 18:05:38 +0800

    add binarytree and ipillegal

--- binarytree.py
+++ binarytree.py
@@ -12,9 +12,9 @@ class node(object):
 def font_traversal(tree):
     if tree:
         if type(tree) == str:
-            print(tree)
+            print(tree, end='')
         else:
-            print(tree.node)
+            print(tree.node, end='')
             font_traversal(tree.left)
             font_traversal(tree.right)
     else:
@@ -23,26 +23,50 @@ def font_traversal(tree):
 
 def mid_traversal(tree):
     if tree:
-        mid_traversal(tree.left)
-        print(tree.node)
-        mid_traversal(tree.right)
+    	if type(tree) == str:
+    		print(tree, end='')
+    	else:
+        	mid_traversal(tree.left)
+        	print(tree.node, end='')
+        	mid_traversal(tree.right)
     else:
         return
 
 
 def later_tarversal(tree):
-    if node:
-        later_tarversal(left)
-        later_tarversal(right)
-        print(node)
+    if tree:
+    	if type(tree) == str:
+    		print(tree, end='')
+    	else:
+        	later_tarversal(tree.left)
+        	later_tarversal(tree.right)
+        	print(tree.node, end='')
     else:
         return
 
+def level_traversal(tree):
+	if tree:
+		queue = [tree]
+		while queue:
+			current = queue.pop(0)
+			if type(current) == str:
+				print(current, end='')
+			else:
+				print(current.node, end='')
+				if current.left:
+					queue.append(current.left)
+				if current.right:
+					queue.append(current.right)
+
 if __name__ == '__main__':
-    tree = node('A', node('B', 'C', 'D'), node('E', 'F', node('G', ' H')))
+    tree = node('A', node('B', 'C', 'D'), node('E', 'F', node('G', 'H')))
     print('the font')
     font_traversal(tree)
+    print('\nthe mid')
     mid_traversal(tree)
+    print('\nthe later')
     later_tarversal(tree)
+    print('\nthe level')
+    level_traversal(tree)
 
 

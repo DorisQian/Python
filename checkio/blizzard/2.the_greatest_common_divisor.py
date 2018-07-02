23ca9ed62bc644e9116e909c35c82653de16df41
DorisQian <qianyh0806@163.com>
2018-07-02 15:09:10 +0800

    high efficiency with github

--- checkio/blizzard/2.the_greatest_common_divisor.py
+++ checkio/blizzard/2.the_greatest_common_divisor.py
@@ -20,13 +20,50 @@ How it is used: GCD is a basic concept found in mathematically oriented software
 Precondition: 
 1 < len(args) ≤ 10
 all(0 < x ≤ 2 ** 32 for x in args)
+
+“[欧几里德算法]是所有算法的祖父，因为它是迄今存活到今天的最古老的非平凡算法。”
+- Donald Knuth，计算机编程艺术，Vol。 2：Seminumerical Algorithms，第2版（1981年）。
+
+最大公约数（GCD）也称为两个或多个整数（其中至少有一个不为零）的最大公因数，是除数之外没有余数的最大正整数。 
+例如，8和12的GCD是4。
+
+给你任意数量的正整数。 你应该找到这些数字的最大公约数。
+
+输入：任意数量的正整数。
+输出：最大公约数为整数。
+
+如何使用它：GCD是数学导向软件中的一个基本概念。 这是一个简单算法的很好的例子，它有许多可能的应用。
 '''
 
 def greatest_common_divisor(*args):
     """
-        Find the greatest common divisor
+    效率低
+    1
+    m = min(args)
+    appro = [i for i in range(1, m + 1) if m % i == 0]
+    appro.reverse()
+
+    for n in appro:
+    	count = 0
+    	for j in args:
+    		if j % n == 0:
+    			count += 1
+    	if count == len(args):
+    		return n
+	2
+    m = min(args)
+    for i in range(m, 0, -1):
+    	if m % i == 0:
+    		count = 0
+    		for n in args:
+    			if n % i == 0:
+    				count += 1
+    		if count == len(args):
+    			return i
     """
-    return 1
+from functools import reduce
+gcd = lambda a, b: a if b == 0 else gcd(b, a % b)
+greatest_common_divisor = lambda *args: reduce(gcd, args)
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing

9e15ab0049e640107f4df4d0545a7e0994701c6d
DorisQian <qianyh0806@163.com>
2018-07-03 16:09:04 +0800

    solve 5

--- checkio/blizzard/5.unix_match_part_1.py
+++ checkio/blizzard/5.unix_match_part_1.py
@@ -1,10 +1,16 @@
 '''
-Sometimes you find yourself in a situation where among a huge number of files on your computer or in a separate folder you need to find files of a certain type - for example, images with the extension '.jpg' or documents with the extension '.txt', or even files that have the word 'butterfly' in their name. Doing this manually can be time-consuming. 'Matching' or patterns for searching files by a specific mask are just what you need for these sort of challenges.
+Sometimes you find yourself in a situation where among a huge number of files on your computer or in a 
+separate folder you need to find files of a certain type - for example, images with the extension '.jpg' 
+or documents with the extension '.txt', or even files that have the word 'butterfly' in their name. 
+Doing this manually can be time-consuming. 'Matching' or patterns for searching files by a specific mask are 
+just what you need for these sort of challenges.
 This mission will help you understand how this works.
 
 You need to find out if the given unix pattern matches the given filename.
 
-Let me show you a couple of small examples of matching the filenames in the unix-shell. For example, * matches everything and *.txt matches all of the files that have txt extension. Here is a small table that shows symbols that can be used in patterns.
+Let me show you a couple of small examples of matching the filenames in the unix-shell. For example, 
+* matches everything and *.txt matches all of the files that have txt extension. Here is a small table that 
+shows symbols that can be used in patterns.
 
 *	matches everything
 ?	matches any single character
@@ -25,16 +31,24 @@ How it is used: in the unix-shell
 
 Precondition: 0 < len(strings) < 100
 '''
-
+import re
 def unix_match(filename: str, pattern: str) -> bool:
-
-    # your code here
-    return filename == pattern
+    #patten = re.compile(pattern.replace('*', '.'))
+    #print(patten)
+    pattern = pattern.replace('.', '\.').replace('*', '.+').replace('?', '.')
+    print(pattern)
+    match_list = re.findall(pattern, filename)
+    match = ''.join(match_list)
+    print(match)
+    if match == filename:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
-    print("Example:")
-    print(unix_match('somefile.txt', '*'))
+    #print("Example:")
+    #print(unix_match('somefile.txt', '*'))
     
     # These "asserts" are used for self-checking and not for an auto-testing
     assert unix_match('somefile.txt', '*') == True

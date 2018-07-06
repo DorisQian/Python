2af96c468bee770b305ae04e7c426e5c5f26f6bd
DorisQian <qianyh0806@163.com>
2018-07-06 16:14:45 +0800

    solve with github

--- checkio/pycontw/1.simple_hashlib.py
+++ checkio/pycontw/1.simple_hashlib.py
@@ -1,9 +1,12 @@
 '''
 In a correctly designed system, a plain password is not stored in the database. Instead, a hash of the password is stored. You are going to verify a password and you will need to use the same one-way function to calculate the hash and do a comparison.
-
+在正确设计的系统中，普通密码不存储在数据库中。
+而是存储密码的哈希值。
+您将验证密码，您将需要使用相同的单向函数来计算哈希值并进行比较。
 Input: Two arguments. A string to be hashed and a hash algorithm as a string (unicode utf8).
-
+输入：两个参数。 要散列的字符串和散列算法作为字符串（unicode utf8）。
 Output: Hexadecimal hash for for input string using given algorithm as a string.
+输出：使用给定算法作为字符串的输入字符串的十六进制哈希
 
 Example:
 
@@ -14,9 +17,9 @@ Precondition:
 algorithm in ("md5", "sha224", "sha256", "sha384", "sha512", "sha1")
 0 ≤ len(hashed_string) ≤ 2**10
 '''
-
+import hashlib
 def checkio(hashed_string, algorithm):
-    return None
+    return getattr(hashlib, algorithm)(hashed_string.encode('utf-8')).hexdigest()
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing

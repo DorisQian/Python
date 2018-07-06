70aaf17136cdb2bdc2d50f4699f5c37eca31d815
DorisQian <qianyh0806@163.com>
2018-07-06 11:16:24 +0800

    solve with github

--- checkio/github/7.the_flat_dictionary.py
+++ checkio/github/7.the_flat_dictionary.py
@@ -47,14 +47,20 @@ root_dictionary != {}
 
 任务处于审阅模式。你看不到解决方案领袖委员会，但你可以在解决任务后通过随机审查看到其他用户解决方案。
 
-尼古拉喜欢把所有的东西都归类在眼前。有一次斯蒂芬为他的生日给了他一个标签制造商，机器人在船上的每一个表面都剥下了几个星期的标签。此后，他将实验室中的所有试剂，图书馆的书籍和书桌上的笔记分类。但后来他学习了python字典，并对索菲亚无人机的所有可能配置进行了分类。现在这些文件被组织在一个深层嵌套的结构中，但是索菲亚不喜欢这个。让我们来帮助索菲亚把这些字典弄平。
+尼古拉喜欢把所有的东西都归类在眼前。有一次斯蒂芬为他的生日给了他一个标签制造商，机器人在船上的每一个表面都剥下了
+几个星期的标签。此后，他将实验室中的所有试剂，图书馆的书籍和书桌上的笔记分类。但后来他学习了python字典，并对索菲亚
+无人机的所有可能配置进行了分类。现在这些文件被组织在一个深层嵌套的结构中，但是索菲亚不喜欢这个。让我们来帮助索菲亚
+把这些字典弄平。
 
-Python字典是一种方便的数据类型来存储和处理配置。它们允许您通过键存储数据来创建嵌套结构。给你一个字典，其中的键是字符串，值是字符串或字典。目标是扁平字典，但保存在键中的结构。结果应该是没有嵌套字典的字典。密钥应该包含包含原始字典中的父密钥的路径。路径中的键由“/”分隔。如果一个值是一个空的字典，那么它应该被一个空字符串（“”）替换。我们来看一个例子：
+Python字典是一种方便的数据类型来存储和处理配置。它们允许您通过键存储数据来创建嵌套结构。给你一个字典，其中的键是字符串，
+值是字符串或字典。目标是扁平字典，但保存在键中的结构。结果应该是没有嵌套字典的字典。密钥应该包含包含原始字典中的父密钥的
+路径。路径中的键由“/”分隔。如果一个值是一个空的字典，那么它应该被一个空字符串（“”）替换。我们来看一个例子：
 
 输入：作为字典的原始字典。
 输出：扁平字典作为字典。
 
-如何使用：如果您需要解析配置文件并简化祖先系统和文件结构的结构，则此概念可能非常有用。您可以根据自己的规格轻松修改这个想法。除此之外，能够阅读代码并搜索错误是一项非常有用的技能。
+如何使用：如果您需要解析配置文件并简化祖先系统和文件结构的结构，则此概念可能非常有用。您可以根据自己的规格轻松修改这个想法。
+此之外，能够阅读代码并搜索错误是一项非常有用的技能。
 
 前提：
 字典中的键是非空字符串。
@@ -62,19 +68,27 @@ Python字典是一种方便的数据类型来存储和处理配置。它们允�
 root_dictionary！= {}
 '''
 
-
 def flatten(dictionary):
-    # your code here
-    return {}
+    temporary = [((), dictionary)]
+    result = {}
+    while temporary:
+        path, value = temporary.pop()
+        for k, v in value.items():
+            if isinstance(v, dict) and v:
+                temporary.append((path + (k,), v))
+            else:
+                result['/'.join(path + (k,))] = v or ''
+    print(result)
+    return result
 
 
 if __name__ == '__main__':
     test_input = {"key": {"deeper": {"more": {"enough": "value"}}}}
-    print(' Input: {}'.format(test_input))
-    print('Output: {}'.format(flatten(test_input)))
+    #print(' Input: {}'.format(test_input))
+    #print('Output: {}'.format(flatten(test_input)))
 
     #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert flatten({"key": "value"}) == {"key": "value"}, "Simple"
+    assert flatten({"key": "value"}) == {'key': 'value'}, "Simple"
     assert flatten(
         {"key": {"deeper": {"more": {"enough": "value"}}}}
     ) == {"key/deeper/more/enough": "value"}, "Nested"
